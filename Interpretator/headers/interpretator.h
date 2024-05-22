e8e95250ba3a3c515b582440ef3007d126f4417e
MilenaArshakyan <133500973+MilenaArshakyan@users.noreply.github.com>
2024-05-22 23:12:48 +0400

    Update interpretator.h

--- Interpretator/headers/interpretator.h
+++ Interpretator/headers/interpretator.h
@@ -21,11 +21,7 @@ public:
 private:
     symtab symbolTable;
     parser codeParser;
-    //std::vector<std::vector<std::string>> parsed_code;
     std::vector<std::string> parsed_code;
-    //std::unordered_set<Object*> objs;//?????
-    //std::unordered_map<std::string, Object*> vars;
-    //exsit in symtab
     std::chrono::steady_clock::time_point last;
   
     void execute(const std::vector<std::string>& tokens);
@@ -37,10 +33,6 @@ private:
     void executeBlock(const std::vector<std::string>& tokens, size_t& index);
     void runLine(const std::vector<std::string>& tokens);
     Object* evaluateExpression(const std::vector<std::string>& tokens, size_t& index);
-    //Object* getObject(const std::string& token);
-    //Object* parseValue(const std::string& token);
-    //bool validVariableName(const std::string& var);
-
 };
 
 #endif // INTERPRETER_H
