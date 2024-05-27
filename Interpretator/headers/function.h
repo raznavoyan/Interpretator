94d91142ad23c82ce139d4df33863e05e30ca0ed
MilenaArshakyan <arshakyanmilena2005@gmail.com>
2024-05-27 20:24:51 +0400

    add file

--- Interpretator/headers/function.h
+++ Interpretator/headers/function.h
@@ -2,14 +2,13 @@
 #define __FUNCTION__H__
 
 #include "headers.h"
-#include "object.h"
 
 class Function
 {
 private:
     std::vector<std::string> argTypes;
     std::vector<std::string> code; 
-    
+
 public:
     Function(std::vector<std::string> code);
     ~Function();
